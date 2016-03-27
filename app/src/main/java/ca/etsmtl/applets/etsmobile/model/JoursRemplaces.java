0ca74c6215f722ca24717eee96281ac909e62c7a
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2016-03-26 21:05:58 -0400

    Syncing JoursRemplaces and cleaning

--- app/src/main/java/ca/etsmtl/applets/etsmobile/model/JoursRemplaces.java
+++ app/src/main/java/ca/etsmtl/applets/etsmobile/model/JoursRemplaces.java
@@ -25,101 +25,106 @@ import com.j256.ormlite.table.DatabaseTable;
 @DatabaseTable(tableName = "jours_remplaces")
 public class JoursRemplaces extends AttributeContainer implements KvmSerializable {
 
-	@DatabaseField(id = true)
-	public String dateOrigine;
+    @DatabaseField(id = true)
+    public String dateOrigine;
 
-	@DatabaseField
-	public String dateRemplacement;
+    @DatabaseField
+    public String dateRemplacement;
 
-	@DatabaseField
-	public String description;
+    @DatabaseField
+    public String description;
 
-	public JoursRemplaces() {
-	}
+    public JoursRemplaces() {
+    }
 
-	public JoursRemplaces(AttributeContainer inObj, ExtendedSoapSerializationEnvelope envelope) {
+    public JoursRemplaces(AttributeContainer inObj, ExtendedSoapSerializationEnvelope envelope) {
 
-		if (inObj == null)
-			return;
+        if (inObj == null)
+            return;
 
-		SoapObject soapObject = (SoapObject) inObj;
+        SoapObject soapObject = (SoapObject) inObj;
 
-		if (soapObject.hasProperty("dateOrigine")) {
-			Object obj = soapObject.getProperty("dateOrigine");
-			if (obj != null && obj.getClass().equals(SoapPrimitive.class)) {
-				SoapPrimitive j = (SoapPrimitive) obj;
-				if (j.toString() != null) {
-					dateOrigine = j.toString();
-				}
-			} else if (obj != null && obj instanceof String) {
-				dateOrigine = (String) obj;
-			}
-		}
-		if (soapObject.hasProperty("dateRemplacement")) {
-			Object obj = soapObject.getProperty("dateRemplacement");
-			if (obj != null && obj.getClass().equals(SoapPrimitive.class)) {
-				SoapPrimitive j = (SoapPrimitive) obj;
-				if (j.toString() != null) {
-					dateRemplacement = j.toString();
-				}
-			} else if (obj != null && obj instanceof String) {
-				dateRemplacement = (String) obj;
-			}
-		}
-		if (soapObject.hasProperty("description")) {
-			Object obj = soapObject.getProperty("description");
-			if (obj != null && obj.getClass().equals(SoapPrimitive.class)) {
-				SoapPrimitive j = (SoapPrimitive) obj;
-				if (j.toString() != null) {
-					description = j.toString();
-				}
-			} else if (obj != null && obj instanceof String) {
-				description = (String) obj;
-			}
-		}
+        if (soapObject.hasProperty("dateOrigine")) {
+            Object obj = soapObject.getProperty("dateOrigine");
+            if (obj != null && obj.getClass().equals(SoapPrimitive.class)) {
+                SoapPrimitive j = (SoapPrimitive) obj;
+                if (j.toString() != null) {
+                    dateOrigine = j.toString();
+                }
+            } else if (obj != null && obj instanceof String) {
+                dateOrigine = (String) obj;
+            }
+        }
+        if (soapObject.hasProperty("dateRemplacement")) {
+            Object obj = soapObject.getProperty("dateRemplacement");
+            if (obj != null && obj.getClass().equals(SoapPrimitive.class)) {
+                SoapPrimitive j = (SoapPrimitive) obj;
+                if (j.toString() != null) {
+                    dateRemplacement = j.toString();
+                }
+            } else if (obj != null && obj instanceof String) {
+                dateRemplacement = (String) obj;
+            }
+        }
+        if (soapObject.hasProperty("description")) {
+            Object obj = soapObject.getProperty("description");
+            if (obj != null && obj.getClass().equals(SoapPrimitive.class)) {
+                SoapPrimitive j = (SoapPrimitive) obj;
+                if (j.toString() != null) {
+                    description = j.toString();
+                }
+            } else if (obj != null && obj instanceof String) {
+                description = (String) obj;
+            }
+        }
 
-	}
+    }
 
-	@Override
-	public Object getProperty(int propertyIndex) {
-		if (propertyIndex == 0) {
-			return dateOrigine;
-		}
-		if (propertyIndex == 1) {
-			return dateRemplacement;
-		}
-		if (propertyIndex == 2) {
-			return description;
-		}
-		return null;
-	}
+    @Override
+    public Object getProperty(int propertyIndex) {
+        if (propertyIndex == 0) {
+            return dateOrigine;
+        }
+        if (propertyIndex == 1) {
+            return dateRemplacement;
+        }
+        if (propertyIndex == 2) {
+            return description;
+        }
+        return null;
+    }
 
-	@Override
-	public int getPropertyCount() {
-		return 3;
-	}
+    @Override
+    public int getPropertyCount() {
+        return 3;
+    }
 
-	@Override
-	public void getPropertyInfo(int propertyIndex, @SuppressWarnings("rawtypes") Hashtable arg1, PropertyInfo info) {
-		if (propertyIndex == +0) {
-			info.type = PropertyInfo.STRING_CLASS;
-			info.name = "dateOrigine";
-			info.namespace = "http://etsmtl.ca/";
-		}
-		if (propertyIndex == +1) {
-			info.type = PropertyInfo.STRING_CLASS;
-			info.name = "dateRemplacement";
-			info.namespace = "http://etsmtl.ca/";
-		}
-		if (propertyIndex == +2) {
-			info.type = PropertyInfo.STRING_CLASS;
-			info.name = "description";
-			info.namespace = "http://etsmtl.ca/";
-		}
-	}
+    @Override
+    public void getPropertyInfo(int propertyIndex, @SuppressWarnings("rawtypes") Hashtable arg1, PropertyInfo info) {
+        if (propertyIndex == +0) {
+            info.type = PropertyInfo.STRING_CLASS;
+            info.name = "dateOrigine";
+            info.namespace = "http://etsmtl.ca/";
+        }
+        if (propertyIndex == +1) {
+            info.type = PropertyInfo.STRING_CLASS;
+            info.name = "dateRemplacement";
+            info.namespace = "http://etsmtl.ca/";
+        }
+        if (propertyIndex == +2) {
+            info.type = PropertyInfo.STRING_CLASS;
+            info.name = "description";
+            info.namespace = "http://etsmtl.ca/";
+        }
+    }
 
-	@Override
-	public void setProperty(int arg0, Object arg1) {
-	}
+    @Override
+    public void setProperty(int arg0, Object arg1) {
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof JoursRemplaces &&
+                ((JoursRemplaces) o).dateOrigine.equals(this.dateOrigine);
+    }
 }
--- app/src/main/java/ca/etsmtl/applets/etsmobile/util/HoraireManager.java
+++ app/src/main/java/ca/etsmtl/applets/etsmobile/util/HoraireManager.java
@@ -2,7 +2,6 @@ package ca.etsmtl.applets.etsmobile.util;
 
 import android.app.Activity;
 import android.os.AsyncTask;
-import android.util.Log;
 
 import com.j256.ormlite.dao.Dao;
 import com.octo.android.robospice.persistence.exception.SpiceException;
@@ -11,19 +10,14 @@ import com.octo.android.robospice.request.listener.RequestListener;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Locale;
 import java.util.Observable;
 
 import ca.etsmtl.applets.etsmobile.db.DatabaseHelper;
 import ca.etsmtl.applets.etsmobile.model.Event;
-import ca.etsmtl.applets.etsmobile.model.EventList;
 import ca.etsmtl.applets.etsmobile.model.HoraireActivite;
-import ca.etsmtl.applets.etsmobile.model.HoraireExamenFinal;
 import ca.etsmtl.applets.etsmobile.model.JoursRemplaces;
 import ca.etsmtl.applets.etsmobile.model.Seances;
-import ca.etsmtl.applets.etsmobile.model.listeDesActivitesEtProf;
-import ca.etsmtl.applets.etsmobile.model.listeHoraireExamensFinaux;
 import ca.etsmtl.applets.etsmobile.model.listeJoursRemplaces;
 import ca.etsmtl.applets.etsmobile.model.listeSeances;
 
@@ -65,20 +59,12 @@ public class HoraireManager extends Observable implements RequestListener<Object
 
             @Override
             protected Void doInBackground(Void... voids) {
-                //listeHoraireEtProf
-                if (o instanceof listeDesActivitesEtProf) {
-                    listeDesActivitesEtProf listeDesActivitesEtProf = (listeDesActivitesEtProf) o;
-
-                    deleteExpiredHoraireActivite(listeDesActivitesEtProf);
-                    createOrUpdateHoraireActiviteInDB(listeDesActivitesEtProf);
-                }
 
                 //lireJoursRemplaces
                 if (o instanceof listeJoursRemplaces) {
                     listeJoursRemplaces listeJoursRemplaces = (listeJoursRemplaces) o;
-
-                    deleteExpiredJoursRemplaces(listeJoursRemplaces);
-                    createOrUpdateJoursRemplacesInDB(listeJoursRemplaces);
+                    new Synchronizer<JoursRemplaces>(daoJoursRemplaces)
+                            .synchronize(listeJoursRemplaces.listeJours);
                     syncJoursRemplacesEnded = true;
                 }
 
@@ -105,8 +91,10 @@ public class HoraireManager extends Observable implements RequestListener<Object
                 /*todo
                 if (o instanceof EventList) {
                     EventList eventList = (EventList) o;
-                    deleteExpiredEvent(eventList);
-                    createOrUpdateEventListInBD(eventList);
+
+                    //Synchronizer
+                    //Don't forget to override "equals" method in Event class to auto sync on id
+
                     syncEventListEnded = true;
                 }
                 //*/
@@ -126,288 +114,6 @@ public class HoraireManager extends Observable implements RequestListener<Object
         }.execute();
 
     }
-
-    /**
-     * Deletes entries in DB that doesn't exist on API
-     *
-     * @param
-     */
-    private void deleteExpiredEvent(EventList envEventList) {
-
-        DatabaseHelper dbHelper = new DatabaseHelper(activity);
-
-        HashMap<String, Event> eventHashMap = new HashMap<String, Event>();
-        for (Event event : envEventList.getEvents()) {
-            eventHashMap.put(event.getId(), event);
-        }
-        ArrayList<Event> dbEvents = new ArrayList<Event>();
-        try {
-            dbEvents = (ArrayList<Event>) dbHelper.getDao(Event.class).queryForAll();
-            for (Event eventsNew : dbEvents) {
-
-                if (!dbEvents.contains(eventsNew.getId())) {
-                    Dao<Event, String> eventDao = dbHelper.getDao(Event.class);
-                    eventDao.deleteById(eventsNew.getId());
-                    Log.v("Supression", eventsNew.getId() + " supprimé");
-                }
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Adds new API entries on DB or updates existing ones
-     *
-     * @param eventList
-     */
-    private void createOrUpdateEventListInBD(EventList eventList) {
-        DatabaseHelper dbHelper = new DatabaseHelper(activity);
-
-        try {
-            for (Event event : eventList.getEvents()) {
-                dbHelper.getDao(Event.class).createOrUpdate(event);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    /**
-     * Deletes entries in DB that doesn't exist on API
-     *
-     * @param listeJoursRemplaces
-     */
-    private void deleteExpiredJoursRemplaces(listeJoursRemplaces listeJoursRemplaces) {
-        DatabaseHelper dbHelper = new DatabaseHelper(activity);
-        HashMap<String, JoursRemplaces> listeJoursRemplacesInAPI = new HashMap<String, JoursRemplaces>();
-
-        //Building the list of entries in API
-        for (JoursRemplaces JoursRemplacesInAPI : listeJoursRemplaces.listeJours) {
-            listeJoursRemplacesInAPI.put(JoursRemplacesInAPI.dateOrigine, JoursRemplacesInAPI);
-        }
-
-        ArrayList<JoursRemplaces> listeJoursRemplacesInDB = new ArrayList<JoursRemplaces>();
-
-        //Comparing entries on DB and API
-        try {
-            listeJoursRemplacesInDB = (ArrayList<JoursRemplaces>) dbHelper.getDao(JoursRemplaces.class).queryForAll();
-
-            for (JoursRemplaces JoursRemplacesInDB : listeJoursRemplacesInDB) {
-
-                if (!listeJoursRemplacesInAPI.containsKey((String) JoursRemplacesInDB.dateOrigine)) {
-                    Dao<JoursRemplaces, String> JoursRemplacesDao = dbHelper.getDao(JoursRemplaces.class);
-
-                    JoursRemplacesDao.deleteById(JoursRemplacesInDB.dateOrigine);
-                    Log.v("Supression", JoursRemplacesInDB.dateOrigine + " supprimé");
-                }
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Adds new API entries on DB or updates existing ones
-     *
-     * @param listeJoursRemplaces
-     */
-    private void createOrUpdateJoursRemplacesInDB(listeJoursRemplaces listeJoursRemplaces) {
-        DatabaseHelper dbHelper = new DatabaseHelper(activity);
-
-        try {
-            for (JoursRemplaces JoursRemplaces : listeJoursRemplaces.listeJours) {
-                dbHelper.getDao(JoursRemplaces.class).createOrUpdate(JoursRemplaces);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Deletes entries in DB that doesn't exist on API
-     *
-     * @param listeSeances
-     */
-    private void deleteExpiredSeances(listeSeances listeSeances) {
-        DatabaseHelper dbHelper = new DatabaseHelper(activity);
-
-        HashMap<String, Seances> listeSeancesInAPI = new HashMap<String, Seances>();
-
-        //Building the list of entries in API
-        for (Seances SeancesInAPI : listeSeances.ListeDesSeances) {
-
-            SeancesInAPI.id = SeancesInAPI.coursGroupe +
-                    SeancesInAPI.dateDebut +
-                    SeancesInAPI.dateFin +
-                    SeancesInAPI.local;
-
-            listeSeancesInAPI.put(SeancesInAPI.id, SeancesInAPI);
-        }
-
-        ArrayList<Seances> listeSeancesInDB = new ArrayList<Seances>();
-
-        //Comparing entries on DB and API
-        try {
-            listeSeancesInDB = (ArrayList<Seances>) dbHelper.getDao(Seances.class).queryForAll();
-
-            for (Seances SeancesInDB : listeSeancesInDB) {
-
-                if (!listeSeancesInAPI.containsKey((String) SeancesInDB.id)) {
-                    Dao<Seances, String> SeancesDao = dbHelper.getDao(Seances.class);
-
-                    SeancesDao.deleteById(SeancesInDB.id);
-                }
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Adds new API entries on DB or updates existing ones
-     *
-     * @param listeSeances
-     */
-    private void createOrUpdateSeancesInDB(listeSeances listeSeances) {
-        DatabaseHelper dbHelper = new DatabaseHelper(activity);
-
-        try {
-            for (Seances Seances : listeSeances.ListeDesSeances) {
-                dbHelper.getDao(Seances.class).createOrUpdate(Seances);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Deletes entries in DB that doesn't exist on API
-     *
-     * @param listeHoraireExamensFinaux
-     */
-    private void deleteExpiredExamensFinaux(listeHoraireExamensFinaux listeHoraireExamensFinaux) {
-        DatabaseHelper dbHelper = new DatabaseHelper(activity);
-
-        HashMap<String, HoraireExamenFinal> listeHoraireExamenFinalInAPI = new HashMap<String, HoraireExamenFinal>();
-
-        //Building the list of entries in API
-        for (HoraireExamenFinal horaireExamenFinalInAPI : listeHoraireExamensFinaux.listeHoraire) {
-
-            horaireExamenFinalInAPI.id = horaireExamenFinalInAPI.sigle + "-" +
-                    horaireExamenFinalInAPI.groupe +
-                    horaireExamenFinalInAPI.dateExamen +
-                    horaireExamenFinalInAPI.heureDebut +
-                    horaireExamenFinalInAPI.heureFin;
-
-            listeHoraireExamenFinalInAPI.put(horaireExamenFinalInAPI.id, horaireExamenFinalInAPI);
-        }
-
-        ArrayList<HoraireExamenFinal> listeHoraireExamenFinalInDB = new ArrayList<HoraireExamenFinal>();
-
-        //Comparing entries on DB and API
-        try {
-            listeHoraireExamenFinalInDB = (ArrayList<HoraireExamenFinal>) dbHelper.getDao(HoraireExamenFinal.class).queryForAll();
-
-            for (HoraireExamenFinal horaireExamenFinalInDB : listeHoraireExamenFinalInDB) {
-
-                if (!listeHoraireExamenFinalInAPI.containsKey((String) horaireExamenFinalInDB.id)) {
-                    Dao<HoraireExamenFinal, String> horaireExamenFinalDao = dbHelper.getDao(HoraireExamenFinal.class);
-
-                    horaireExamenFinalDao.deleteById(horaireExamenFinalInDB.id);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Adds new API entries on DB or updates existing ones
-     *
-     * @param listeHoraireExamensFinaux
-     */
-    private void createOrUpdateExamensFinauxInDB(listeHoraireExamensFinaux listeHoraireExamensFinaux) {
-        DatabaseHelper dbHelper = new DatabaseHelper(activity);
-
-        try {
-            for (HoraireExamenFinal horaireExamenFinal : listeHoraireExamensFinaux.listeHoraire) {
-                dbHelper.getDao(HoraireExamenFinal.class).createOrUpdate(horaireExamenFinal);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Deletes entries in DB that doesn't exist on API
-     *
-     * @param listeDesActivitesEtProf API list
-     */
-    private void deleteExpiredHoraireActivite(listeDesActivitesEtProf listeDesActivitesEtProf) {
-        DatabaseHelper dbHelper = new DatabaseHelper(activity);
-
-        HashMap<String, HoraireActivite> listeHoraireActiviteInAPI = new HashMap<String, HoraireActivite>();
-
-        //Building the list of entries in API
-        for (HoraireActivite horaireActiviteInAPI : listeDesActivitesEtProf.listeActivites) {
-            horaireActiviteInAPI.id = "" + horaireActiviteInAPI.sigle +
-                    horaireActiviteInAPI.groupe +
-                    horaireActiviteInAPI.jour +
-                    horaireActiviteInAPI.heureDebut +
-                    horaireActiviteInAPI.heureFin;
-
-            listeHoraireActiviteInAPI.put(horaireActiviteInAPI.id, horaireActiviteInAPI);
-        }
-
-        ArrayList<HoraireActivite> listeHoraireActiviteInDB = new ArrayList<HoraireActivite>();
-
-        //Comparing entries on DB and API
-        try {
-            listeHoraireActiviteInDB = (ArrayList<HoraireActivite>) dbHelper.getDao(HoraireActivite.class).queryForAll();
-
-            for (HoraireActivite horaireActiviteInDB : listeHoraireActiviteInDB) {
-
-                if (!listeHoraireActiviteInAPI.containsKey((String) horaireActiviteInDB.id)) {
-                    Dao<HoraireActivite, String> horaireActiviteDao = dbHelper.getDao(HoraireActivite.class);
-
-                    horaireActiviteDao.deleteById(horaireActiviteInDB.id);
-                    Log.v("Supression", horaireActiviteInDB.id + " supprimé");
-                }
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    /**
-     * Adds new API entries on DB or updates existing ones
-     *
-     * @param listeDesActivitesEtProf API list
-     */
-    private void createOrUpdateHoraireActiviteInDB(listeDesActivitesEtProf listeDesActivitesEtProf) {
-        DatabaseHelper dbHelper = new DatabaseHelper(activity);
-
-        try {
-            for (HoraireActivite horaireActivite : listeDesActivitesEtProf.listeActivites) {
-                dbHelper.getDao(HoraireActivite.class).createOrUpdate(horaireActivite);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     public void updateCalendar() throws Exception {
 
